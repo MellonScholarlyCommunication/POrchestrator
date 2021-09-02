c7f3656f1a5fe2b37c42154bbbf40b8145b0ba6e
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2021-09-02 09:14:54 +0200

    Supporting providing a rule directory as input instread of individual files

--- bin/orchestrator.sh
+++ bin/orchestrator.sh
@@ -4,4 +4,9 @@ PROJDIR=$(dirname $0)
 
 cd ${PROJDIR}/..
 
-./prolog/orchestrator.pl -- "$@"
+# If we get a directory as second argument, glob over all rule files 
+if [ -d $2 ]; then
+    ./prolog/orchestrator.pl -- $1 $2/*.n3
+else
+    ./prolog/orchestrator.pl -- "$@"
+fi
